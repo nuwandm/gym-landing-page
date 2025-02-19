08efd828749140f4b1bdd9f4c5584b94856937bd
Nuwan Madusanka <56627412+nuwandm@users.noreply.github.com>
2025-02-19 22:14:55 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1 class="text-xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+  @reference "tailwindcss/theme";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
