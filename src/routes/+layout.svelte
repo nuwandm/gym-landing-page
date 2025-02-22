aa3771d331a737fe1c2d259e8123454e8bda8011
Nuwan Madusanka <56627412+nuwandm@users.noreply.github.com>
2025-02-22 12:40:00 +0530

    make navbar reactive to the scrolling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script>
-  let { children } = $props();
   import "../app.css";
   import CtAs from "../components/CTAs.svelte";
   import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+
   import { openModal } from "../store/store";
 
-  const reroute = (href) => {
-    openModal.set(false);
+  let y;
+  $: outerHeight = 0;
+
+  function reroute(href) {
+    $openModal = false;
     window.location.href = href;
-  };
+  }
 </script>
 
 {#if $openModal}
@@ -17,10 +21,10 @@
   >
     <div class="flex items-center justify-between gap-4 border-b pb-2">
       <h1 class="font-semibold">
-        Max <span class="text-indigo-400">Fit</span>
+        Mac <span class="text-indigo-400">Fit</span>
       </h1>
       <button
-        onclick={() => openModal.set(false)}
+        onclick={() => ($openModal = false)}
         class="outline-none border-none"
         aria-label="Close menu"
       >
@@ -42,8 +46,8 @@
       >
         <p class="duration-200 group-hover:pl-2 poppins text-3xl font-semibold">
           Reviews <i class="fa-solid fa-chevron-right text-xl pl-4"></i>
-        </p></button
-      >
+        </p>
+      </button>
       <button
         onclick={() => reroute("#faqs")}
         class="border-none outline-none p-2 group duration-200 cursor-pointer text-left"
@@ -59,6 +63,20 @@
   </div>
 {/if}
 
-{@render children()}
+{#if y > outerHeight}
+  <div
+    class="bg-white fixed top-0 left-0 w-full flex flex-col z-20 px-4 fadeIn"
+  >
+    <Header />
+  </div>
+{/if}
+<slot />
 <Footer />
-<!-- <svelte:window bind:scrollY={y} bind:outerHeight /> -->
+<svelte:window bind:scrollY={y} bind:outerHeight />
+<!-- header
+hero
+product description
+user reviews
+faq
+conversion-
+footer -->
